c11027f172b70dcc124a592bfde330a783bff712
DucVanNam <subinducnam@gmail.com>
2022-10-16 22:49:00 +0700

    doing

--- src/app/pages/datvexe/datvexe.component.ts
+++ src/app/pages/datvexe/datvexe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-datvexe',
@@ -13,39 +14,63 @@ export class DatvexeComponent implements OnInit {
       name: 'Chuyến xe số 1',
       diemdi: 'Hà Nội',
       diemtoi: 'Lào Cai',
-      loaixe: '16 chỗ'
+      loaixe: '16 chỗ',
+      giave: '100000',
+      giodi: '12:00',
+      gioden: '13:50',
+      ghetrong: 10
     },
     {
       id: 2,
       name: 'Chuyến xe số 2',
       diemdi: 'Hà Nội',
       diemtoi: 'Bắc Giang',
-      loaixe: '30 chỗ'
+      loaixe: '30 chỗ',
+      giave: '50000',
+      giodi: '12:00',
+      gioden: '13:50',
+      ghetrong: 10
     },
     {
       id: 3,
       name: 'Chuyến xe số 3',
       diemdi: 'Hà Nội',
       diemtoi: 'Hải Phòng',
-      loaixe: '30 chỗ'
+      loaixe: '30 chỗ',
+      giave: '70000',
+      giodi: '12:00',
+      gioden: '13:50',
+      ghetrong: 10
     },
     {
       id: 4,
       name: 'Chuyến xe số 4',
       diemdi: 'Hà Nội',
       diemtoi: 'Quảng Ninh',
-      loaixe: '16 chỗ'
+      loaixe: '16 chỗ',
+      giave: '100000',
+      giodi: '12:00',
+      gioden: '13:50',
+      ghetrong: 10
     },
     {
       id: 5,
       name: 'Chuyến xe số 5',
       diemdi: 'Hà Nội',
       diemtoi: 'Cần Thơ',
-      loaixe: '30 chỗ'
+      loaixe: '30 chỗ',
+      giave: '90000',
+      giodi: '12:00',
+      gioden: '13:50',
+      ghetrong: 10
     }
   ];
   isVisible = false;
+
+  selectedValue = null;
+
   constructor() { }
+
 
   ngOnInit(): void {
   }
